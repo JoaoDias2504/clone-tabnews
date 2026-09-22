1373ee1ac9b7008ab7e8d05b6fea741a8f6645ed
JoaoDias2504 <dias.joao@usp.br>
2026-09-22 16:05:39 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>O amor tudo sofre, tudo crê, tudo espera, tudo suporta.</h1>
+    return <h1>O amor tudo sofre, tudo crê, tudo espera, tudo suporta. 1Co 13.7</h1>
 }
 
 export default Home;
